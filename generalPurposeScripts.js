9e6d15a2792c7446afefc6575ee15b0aed069e93
keith72632 <keithhorton30@outlook.com>
2021-09-13 17:12:12 -0500

    added fluoride

--- generalPurposeScripts.js
+++ generalPurposeScripts.js
@@ -1,6 +1,6 @@
-/*
- * Pass the variable(key) for data point established in the Meta Data Point tab to 
- * this function. This can only take 'actual' data points. Can not take other Meta 
+ /*
+ * Pass the variable(key) for data point established in the Meta Data Point tab to
+ * this function. This can only take 'actual' data points. Can not take other Meta
  * points.
  */
 
@@ -16,15 +16,37 @@ function getMidnightValue(dpoint)
 }
 
 /*
- * Logs the max flow in gallons per minute starting from 00:00 hours to current
+ * Logs the max flow in gallons per minute starting from $(date-1)T-00:00 to $(date)T-00:00. Operators will record this number
+ * after clock hits midnight, so data must range from present time(00:00) to the midnight before.
  */
 function getPeakFlow(dpoint)
 {
     var list = [];
-    var start = new Date();
-    var fin = Date.now();
-    start.setHours(0, 0, 0 , 0);
-    var mgd = dpoint.getStats(start.getTime(), fin).maximumValue;
-    var gpm = (mgd * 1000000) / 1440;
+    var fin = new Date();
+    var day_range = (1000 * 60) * 1440; //24 hours
+    fin.setHours(0, 0, 0 , 0); //sets end of range to midnight
+
+    var start = new Date(fin.getTime() - day_range);
+
+    var mgd = dpoint.getStats(start.getTime(), fin.getTime()).maximumValue;
+    var gpm = mgd_to_gpm(mgd);
     return gpm;
+}
+
+function mgd_to_gpm(value)
+{
+    return (value * 1000000) / 1440;
+}
+
+function fluoride(dpoint)
+{
+    var fin = new Date();
+    var day_range = (1000 * 60) * 1440; //24 hours
+    fin.setHours(0, 0, 0 , 0); //sets end of range to midnight
+
+    var start = new Date(fin.getTime() - day_range);
+
+    var first = dpoint.getStats(start.getTime(), fin.getTime()).firstValue;
+    var last = dpoint.getStats(start.getTime(), fin.getTime()).lastValue;
+    return last;
 }
